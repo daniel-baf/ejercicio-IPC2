89c3d61311785e436495a3f93a51faccb62c4770
Daniel B <danibaufu@gmail.com>
2020-09-03 19:58:26 -0600

    cambios en las llaves foraneas

--- avt.sql
+++ avt.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `aviateca`;
-USE `aviateca`;
+CREATE DATABASE  IF NOT EXISTS `aviteca`;
+USE `aviteca`;
 --
 -- Table structure for table `aeropuerto`
 --
@@ -12,7 +12,7 @@ CREATE TABLE `aeropuerto` (
   `Localidad` varchar(60) NOT NULL,
   `Pais` varchar(45) NOT NULL,
   PRIMARY KEY (`Codigo`)
-);
+) ;
 
 --
 -- Table structure for table `avion`
@@ -45,13 +45,13 @@ DROP TABLE IF EXISTS `piso`;
 
 CREATE TABLE `piso` (
   `Codigo` varchar(30) NOT NULL,
-  `n_filas` int NOT NULL,
-  `n_columnas` int NOT NULL,
+  `n_filas` int(11) NOT NULL,
+  `n_columnas` int(11) NOT NULL,
   `Codigo_avion` varchar(30) NOT NULL,
   PRIMARY KEY (`Codigo`),
   KEY `avionsPiso_FK` (`Codigo_avion`),
   CONSTRAINT `avionsPiso_FK` FOREIGN KEY (`Codigo_avion`) REFERENCES `avion` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ;
 
 --
 -- Table structure for table `asiento`
@@ -61,14 +61,15 @@ DROP TABLE IF EXISTS `asiento`;
 
 CREATE TABLE `asiento` (
   `Codigo` varchar(30) NOT NULL,
-  `fila` varchar(45) DEFAULT NULL,
-  `columna` varchar(45) DEFAULT NULL,
-  `Codigo_piso` varchar(45) DEFAULT NULL,
-  `Codigo_clase` VARCHAR (30) NOT NULL
+  `fila` varchar(45) NOT NULL,
+  `columna` varchar(45) NOT NULL,
+  `Codigo_piso` varchar(30) NOT NULL,
+  `Codigo_clase` varchar(30) NOT NULL,
   PRIMARY KEY (`Codigo`),
   KEY `asientoPiso_fk` (`Codigo_piso`),
-  CONSTRAINT `asientoPiso_fk` FOREIGN KEY (`Codigo_piso`) REFERENCES `piso` (`Codigo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `asientoClase_fk` FOREIGN KEY (`Codigo_clase`) REFERENCES `clase` (`Codigo`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  KEY `asientoClase_fk` (`Codigo_clase`),
+  CONSTRAINT `asientoClase_fk` FOREIGN KEY (`Codigo_clase`) REFERENCES `clase` (`Codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `asientoPiso_fk` FOREIGN KEY (`Codigo_piso`) REFERENCES `piso` (`Codigo`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 --
@@ -79,7 +80,7 @@ DROP TABLE IF EXISTS `avion_clase`;
 
 CREATE TABLE `avion_clase` (
   `Codigo` varchar(30) NOT NULL,
-  `Cantidad_asientos` int NOT NULL,
+  `Cantidad_asientos` int(11) NOT NULL,
   `Codigo_clase` varchar(45) NOT NULL,
   `Codigo_avion` varchar(45) NOT NULL,
   PRIMARY KEY (`Codigo`),
@@ -101,9 +102,9 @@ CREATE TABLE `cliente` (
   `Apellido` varchar(45) NOT NULL,
   `Direccion` varchar(90) NOT NULL,
   `Telefono` varchar(45) NOT NULL,
-  `Tarjeta` varchar(45) DEFAULT NULL,
+  `Tarjeta` varchar(45) NOT NULL,
   PRIMARY KEY (`DPI`)
-);
+) ;
 
 --
 -- Table structure for table `vuelo`
@@ -116,9 +117,9 @@ CREATE TABLE `vuelo` (
   `Fecha_vuelo` date NOT NULL,
   `Hora_salida` varchar(20) NOT NULL,
   `Hora_entrada` varchar(20) NOT NULL,
-  `Aeropuerto_entrada` varchar(30) DEFAULT NULL,
-  `Aeropuerto_salida` varchar(30) DEFAULT NULL,
-  `Codigo_Avion` varchar(30) DEFAULT NULL,
+  `Aeropuerto_entrada` varchar(30) NOT NULL,
+  `Aeropuerto_salida` varchar(30) NOT NULL,
+  `Codigo_Avion` varchar(30) NOT NULL,
   PRIMARY KEY (`Codigo`),
   KEY `aeropuertoEntradaVuelo_FK` (`Aeropuerto_entrada`),
   KEY `aeropuertoSalidaVuelo_FK` (`Aeropuerto_salida`),
@@ -135,10 +136,10 @@ CREATE TABLE `vuelo` (
 DROP TABLE IF EXISTS `tarjeta_embarque`;
 
 CREATE TABLE `tarjeta_embarque` (
-  `ID` int NOT NULL,
+  `ID` int(11) NOT NULL,
   `Tipo_pago` varchar(45) NOT NULL,
   `DPI_cliente` varchar(45) NOT NULL,
-  `Codigo_vuelo` varchar(35) DEFAULT NULL,
+  `Codigo_vuelo` varchar(35) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `tarjetaCliente_FK` (`DPI_cliente`),
   KEY `tarjetaVuelo_FK` (`Codigo_vuelo`),
@@ -153,14 +154,13 @@ CREATE TABLE `tarjeta_embarque` (
 DROP TABLE IF EXISTS `reserva_viaje`;
 
 CREATE TABLE `reserva_viaje` (
-  `No_reserva` int NOT NULL AUTO_INCREMENT,
+  `No_reserva` int(11) NOT NULL AUTO_INCREMENT,
   `Codigo_asiento` varchar(30) NOT NULL,
-  `Codigo_tarjeta` int NOT NULL,
+  `Codigo_tarjeta` int(11) NOT NULL,
   `Fecha_reserva` date DEFAULT NULL,
   PRIMARY KEY (`No_reserva`),
   KEY `asientoReserva_fk` (`Codigo_asiento`),
-  KEY `claseReserva_fk` (`Clase`),
   KEY `tarjetaReserva_fk` (`Codigo_tarjeta`),
-  CONSTRAINT `asientoReserva_fk` FOREIGN KEY (`Codigo_asiento`) REFERENCES `asiento` (`Codigo`) ON UPDATE CASCADE,
-  CONSTRAINT `tarjetaReserva_fk` FOREIGN KEY (`Codigo_tarjeta`) REFERENCES `tarjeta_embarque` (`ID`) ON UPDATE CASCADE
+  CONSTRAINT `asientoReserva_fk` FOREIGN KEY (`Codigo_asiento`) REFERENCES `asiento` (`Codigo`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `tarjetaReserva_fk` FOREIGN KEY (`Codigo_tarjeta`) REFERENCES `tarjeta_embarque` (`ID`) ON DELETE NO ACTION ON UPDATE CASCADE
 );
