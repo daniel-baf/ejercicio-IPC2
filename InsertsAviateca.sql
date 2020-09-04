7499d9274b6c3317bb2c1b32ad0f31683a9ed83a
Daniel B <danibaufu@gmail.com>
2020-09-03 18:47:24 -0600

    primer commit

---
+++ InsertsAviateca.sql
@@ -0,0 +1,42 @@
+
+
+INSERT INTO cliente VALUES ('1000','JUAN','LOPEZ','Quetzaltenango','11111111',null);
+INSERT INTO cliente VALUES ('1001','MARIA','GARCIA','Zacapa','22222222',null);
+INSERT INTO cliente VALUES ('1002','FRANCISCO','MARTINEZ','Quetzaltenango','33333333',null);
+INSERT INTO cliente VALUES ('1003','MANUEL','SAEZ','San Marcos','44444444',null);
+INSERT INTO cliente VALUES ('1004','ANTONIO','GONZALEZ','Guatemala','55555555',null);
+
+INSERT INTO aeropuerto VALUES ('AER-1','Aurora','Guatemala','Guatemala');
+INSERT INTO aeropuerto VALUES ('AER-2','Haneda','Tokio','Japon');
+INSERT INTO aeropuerto VALUES ('AER-3','Kongsito','Hong Kong','China');
+INSERT INTO aeropuerto VALUES ('AER-4','Munichon','Munich','Alemania');
+
+INSERT INTO clase VALUES ('CLA-1','Ejecutiva');
+INSERT INTO clase VALUES ('CLA-2','Premium');
+INSERT INTO clase VALUES ('CLA-3','Turista');
+INSERT INTO clase VALUES ('CLA-4','Primera Clase');
+
+INSERT INTO avion VALUES ('AVI-1');
+INSERT INTO avion VALUES ('AVI-2');
+INSERT INTO avion VALUES ('AVI-3');
+
+INSERT INTO piso VALUES ('PIS-1','2','3','AVI-1');
+INSERT INTO piso VALUES ('PIS-2','3','6','AVI-1');
+INSERT INTO piso VALUES ('PIS-3','6','1','AVI-2');
+
+INSERT INTO asiento VALUES ('AS-1','1','1','PIS-1');
+INSERT INTO asiento VALUES ('AS-2','1','2','PIS-1');
+INSERT INTO asiento VALUES ('AS-3','1','3','PIS-1');
+INSERT INTO asiento VALUES ('AS-4','2','1','PIS-1');
+INSERT INTO asiento VALUES ('AS-5','2','2','PIS-1');
+INSERT INTO asiento VALUES ('AS-6','1','1','PIS-3');
+INSERT INTO asiento VALUES ('AS-7','2','1','PIS-3');
+INSERT INTO asiento VALUES ('AS-8','3','1','PIS-3');
+
+INSERT INTO vuelo VALUES ('VUL-1',2020-01-01,'01:00','12:00','AER-1','AER-2','AVI-1');
+INSERT INTO vuelo VALUES ('VUL-2',2020-01-02,'01:00','12:00','AER-2','AER-1','AVI-1');
+INSERT INTO vuelo VALUES ('VUL-3',2020-01-01,'01:00','12:00','AER-3','AER-4','AVI-2');
+INSERT INTO vuelo VALUES ('VUL-4',2020-01-02,'01:00','12:00','AER-4','AER-3','AVI-2');
+INSERT INTO vuelo VALUES ('VUL-5',2020-01-03,'01:00','12:00','AER-1','AER-4','AVI-1');
+
+
---
+++ avt.sql
@@ -0,0 +1,166 @@
+CREATE DATABASE  IF NOT EXISTS `aviateca`;
+USE `aviateca`;
+--
+-- Table structure for table `aeropuerto`
+--
+
+DROP TABLE IF EXISTS `aeropuerto`;
+
+CREATE TABLE `aeropuerto` (
+  `Codigo` varchar(30) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Localidad` varchar(60) NOT NULL,
+  `Pais` varchar(45) NOT NULL,
+  PRIMARY KEY (`Codigo`)
+);
+
+--
+-- Table structure for table `avion`
+--
+
+DROP TABLE IF EXISTS `avion`;
+
+CREATE TABLE `avion` (
+  `codigo` varchar(30) NOT NULL,
+  PRIMARY KEY (`codigo`)
+);
+
+--
+-- Table structure for table `clase`
+--
+
+DROP TABLE IF EXISTS `clase`;
+
+CREATE TABLE `clase` (
+  `Codigo` varchar(30) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`Codigo`)
+);
+
+--
+-- Table structure for table `piso`
+--
+
+DROP TABLE IF EXISTS `piso`;
+
+CREATE TABLE `piso` (
+  `Codigo` varchar(30) NOT NULL,
+  `n_filas` int NOT NULL,
+  `n_columnas` int NOT NULL,
+  `Codigo_avion` varchar(30) NOT NULL,
+  PRIMARY KEY (`Codigo`),
+  KEY `avionsPiso_FK` (`Codigo_avion`),
+  CONSTRAINT `avionsPiso_FK` FOREIGN KEY (`Codigo_avion`) REFERENCES `avion` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table `asiento`
+--
+
+DROP TABLE IF EXISTS `asiento`;
+
+CREATE TABLE `asiento` (
+  `Codigo` varchar(30) NOT NULL,
+  `fila` varchar(45) DEFAULT NULL,
+  `columna` varchar(45) DEFAULT NULL,
+  `Codigo_piso` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Codigo`),
+  KEY `asientoPiso_fk` (`Codigo_piso`),
+  CONSTRAINT `asientoPiso_fk` FOREIGN KEY (`Codigo_piso`) REFERENCES `piso` (`Codigo`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table `avion_clase`
+--
+
+DROP TABLE IF EXISTS `avion_clase`;
+
+CREATE TABLE `avion_clase` (
+  `Codigo` varchar(30) NOT NULL,
+  `Cantidad_asientos` int NOT NULL,
+  `Codigo_clase` varchar(45) NOT NULL,
+  `Codigo_avion` varchar(45) NOT NULL,
+  PRIMARY KEY (`Codigo`),
+  KEY `claseClaseAvion_fk` (`Codigo_clase`),
+  KEY `avionClase_fk` (`Codigo_avion`),
+  CONSTRAINT `avionClase_fk` FOREIGN KEY (`Codigo_avion`) REFERENCES `avion` (`codigo`) ON UPDATE CASCADE,
+  CONSTRAINT `claseClaseAvion_fk` FOREIGN KEY (`Codigo_clase`) REFERENCES `clase` (`Codigo`) ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
+  `DPI` varchar(45) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellido` varchar(45) NOT NULL,
+  `Direccion` varchar(90) NOT NULL,
+  `Telefono` varchar(45) NOT NULL,
+  `Tarjeta` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`DPI`)
+);
+
+--
+-- Table structure for table `vuelo`
+--
+
+DROP TABLE IF EXISTS `vuelo`;
+
+CREATE TABLE `vuelo` (
+  `Codigo` varchar(35) NOT NULL,
+  `Fecha_vuelo` date NOT NULL,
+  `Hora_salida` varchar(20) NOT NULL,
+  `Hora_entrada` varchar(20) NOT NULL,
+  `Aeropuerto_entrada` varchar(30) DEFAULT NULL,
+  `Aeropuerto_salida` varchar(30) DEFAULT NULL,
+  `Codigo_Avion` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`Codigo`),
+  KEY `aeropuertoEntradaVuelo_FK` (`Aeropuerto_entrada`),
+  KEY `aeropuertoSalidaVuelo_FK` (`Aeropuerto_salida`),
+  KEY `avionVuelo_FK` (`Codigo_Avion`),
+  CONSTRAINT `aeropuertoEntradaVuelo_FK` FOREIGN KEY (`Aeropuerto_entrada`) REFERENCES `aeropuerto` (`Codigo`) ON UPDATE CASCADE,
+  CONSTRAINT `aeropuertoSalidaVuelo_FK` FOREIGN KEY (`Aeropuerto_salida`) REFERENCES `aeropuerto` (`Codigo`) ON UPDATE CASCADE,
+  CONSTRAINT `avionVuelo_FK` FOREIGN KEY (`Codigo_Avion`) REFERENCES `avion` (`codigo`) ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table `tarjeta_embarque`
+--
+
+DROP TABLE IF EXISTS `tarjeta_embarque`;
+
+CREATE TABLE `tarjeta_embarque` (
+  `ID` int NOT NULL,
+  `Tipo_pago` varchar(45) NOT NULL,
+  `DPI_cliente` varchar(45) NOT NULL,
+  `Codigo_vuelo` varchar(35) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `tarjetaCliente_FK` (`DPI_cliente`),
+  KEY `tarjetaVuelo_FK` (`Codigo_vuelo`),
+  CONSTRAINT `tarjetaCliente_FK` FOREIGN KEY (`DPI_cliente`) REFERENCES `cliente` (`DPI`) ON UPDATE CASCADE,
+  CONSTRAINT `tarjetaVuelo_FK` FOREIGN KEY (`Codigo_vuelo`) REFERENCES `vuelo` (`Codigo`) ON UPDATE CASCADE
+);
+
+--
+-- Table structure for table `reserva_viaje`
+--
+
+DROP TABLE IF EXISTS `reserva_viaje`;
+
+CREATE TABLE `reserva_viaje` (
+  `No_reserva` int NOT NULL AUTO_INCREMENT,
+  `Clase` varchar(30) NOT NULL,
+  `Codigo_asiento` varchar(30) NOT NULL,
+  `Codigo_tarjeta` int NOT NULL,
+  `Fecha_reserva` date DEFAULT NULL,
+  PRIMARY KEY (`No_reserva`),
+  KEY `asientoReserva_fk` (`Codigo_asiento`),
+  KEY `claseReserva_fk` (`Clase`),
+  KEY `tarjetaReserva_fk` (`Codigo_tarjeta`),
+  CONSTRAINT `asientoReserva_fk` FOREIGN KEY (`Codigo_asiento`) REFERENCES `asiento` (`Codigo`) ON UPDATE CASCADE,
+  CONSTRAINT `claseReserva_fk` FOREIGN KEY (`Clase`) REFERENCES `clase` (`Codigo`) ON UPDATE CASCADE,
+  CONSTRAINT `tarjetaReserva_fk` FOREIGN KEY (`Codigo_tarjeta`) REFERENCES `tarjeta_embarque` (`ID`) ON UPDATE CASCADE
+);
