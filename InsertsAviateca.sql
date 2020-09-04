11e6fc97818f8415f83561f7e5a187aa0a975158
William <willy2000.wld@gmail.com>
2020-09-03 19:31:16 -0600

    Se modificaron inserciones de asiento

--- InsertsAviateca.sql
+++ InsertsAviateca.sql
@@ -24,14 +24,14 @@ INSERT INTO piso VALUES ('PIS-1','2','3','AVI-1');
 INSERT INTO piso VALUES ('PIS-2','3','6','AVI-1');
 INSERT INTO piso VALUES ('PIS-3','6','1','AVI-2');
 
-INSERT INTO asiento VALUES ('AS-1','1','1','PIS-1');
-INSERT INTO asiento VALUES ('AS-2','1','2','PIS-1');
-INSERT INTO asiento VALUES ('AS-3','1','3','PIS-1');
-INSERT INTO asiento VALUES ('AS-4','2','1','PIS-1');
-INSERT INTO asiento VALUES ('AS-5','2','2','PIS-1');
-INSERT INTO asiento VALUES ('AS-6','1','1','PIS-3');
-INSERT INTO asiento VALUES ('AS-7','2','1','PIS-3');
-INSERT INTO asiento VALUES ('AS-8','3','1','PIS-3');
+INSERT INTO asiento VALUES ('AS-1','1','1','PIS-1','CLA-1');
+INSERT INTO asiento VALUES ('AS-2','1','2','PIS-1','CLA-1');
+INSERT INTO asiento VALUES ('AS-3','1','3','PIS-1','CLA-1');
+INSERT INTO asiento VALUES ('AS-4','2','1','PIS-1','CLA-2');
+INSERT INTO asiento VALUES ('AS-5','2','2','PIS-1','CLA-2');
+INSERT INTO asiento VALUES ('AS-6','1','1','PIS-3','CLA-1');
+INSERT INTO asiento VALUES ('AS-7','2','1','PIS-3','CLA-1');
+INSERT INTO asiento VALUES ('AS-8','3','1','PIS-3','CLA-1');
 
 INSERT INTO vuelo VALUES ('VUL-1',2020-01-01,'01:00','12:00','AER-1','AER-2','AVI-1');
 INSERT INTO vuelo VALUES ('VUL-2',2020-01-02,'01:00','12:00','AER-2','AER-1','AVI-1');
@@ -43,5 +43,3 @@ INSERT INTO tarjeta_embarque VALUES (1,'Efectivo','1000','VUL-1');
 INSERT INTO tarjeta_embarque VALUES (2,'Efectivo','1002','VUL-1');
 INSERT INTO tarjeta_embarque VALUES (3,'Efectivo','1003','VUL-2');
 INSERT INTO tarjeta_embarque VALUES (4,'Efectivo','1004','VUL-2');
-
-INSERT INTO reserva_viaje VALUES (1,'CLA-1','1003','VUL-2');
